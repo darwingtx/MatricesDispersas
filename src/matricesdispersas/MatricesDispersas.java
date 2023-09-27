cfea046411a3cf9a22db4148091c97b4838a4cf7
DarwinC <dc162005@gmail.com>
2023-09-26 19:56:53 -0500

    Agregue llenado aleatorio de matriz

--- src/matricesdispersas/MatricesDispersas.java
+++ src/matricesdispersas/MatricesDispersas.java
@@ -4,8 +4,6 @@
  */
 package matricesdispersas;
 
-
-
 /**
  *
  * @author abela
@@ -17,28 +15,50 @@ public class MatricesDispersas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         int Mat[][]={{0,0,1},{0,2,4},{1,1,3},{1,2,6},{2,2,9},{3,3,9}};
-       Tripleta T = new Tripleta(ContarDato(Mat));
-       T.CrearTripleta(Mat);
-       
-       Forma1 F1 = new Forma1();
-       F1.Crear(Mat);
-       
-       
-    }
-    public static int  ContarDato(int Mat[][]){
-        int cont=0;
-    for(int i=0;i<Mat.length;i++){
-    for(int j=0;j<Mat[0].length;j++){
-    if(Mat[i][j]!=0){
-    cont++;
-    }
-    
-    }
-    }
-        
-    return cont;
-    }
-    }
-    
 
+        int n = 0;
+        int m = 0;
+        do {
+            n = (int) Math.floor(Math.random() * 10 + 2);
+            m = (int) Math.floor(Math.random() * 10 + 2);
+        } while (n == 0 && m == 0);
+
+        System.out.println(n);
+        System.out.println(m);
+        System.out.println("");
+        int Mat[][] = new int[n][m];
+
+        for (int i = 0; i <= ((n * m) / 2) + 1; i++) {
+            Mat[(int) Math.floor(Math.random() * n)][(int) Math.floor(Math.random() * m)] = (int) Math
+                    .floor(Math.random() * 100 + 1);
+        }
+
+        for (int i = 0; i < Mat.length; i++) {
+            for (int j = 0; j < Mat[i].length; j++) {
+                System.out.print(Mat[i][j] + " ");
+            }
+            System.out.println(); // Salto de línea al final de cada fila
+        }
+        // int Mat[][]={{0,0,1},{0,2,4},{1,1,3},{1,2,6},{2,2,9},{3,3,9}};
+        Tripleta T = new Tripleta(ContarDato(Mat));
+        T.CrearTripleta(Mat);
+
+        Forma1 F1 = new Forma1();
+        F1.Crear(Mat);
+
+    }
+
+    public static int ContarDato(int Mat[][]) {
+        int cont = 0;
+        for (int i = 0; i < Mat.length; i++) {
+            for (int j = 0; j < Mat[0].length; j++) {
+                if (Mat[i][j] != 0) {
+                    cont++;
+                }
+
+            }
+        }
+
+        return cont;
+    }
+}
