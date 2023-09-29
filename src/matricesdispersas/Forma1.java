04e94adf0e4ef53d800499b00cbe97cff24c0a04
Darwin Castaño <dc162005@gmail.com>
2023-09-29 08:42:17 -0500

    Realice el paso 1 y paso 2 estan bien hechos

--- src/matricesdispersas/Forma1.java
+++ src/matricesdispersas/Forma1.java
@@ -16,31 +16,79 @@ public class Forma1 {
       Paso2(M);
       Paso3();
     }
-    
+    public void InsertarFinalRP(int fila, int colm, int dato) {
+
+        Nodo x = new Nodo(fila, colm, dato);
+
+        if (Punta != null) {
+
+            Nodo p = Punta;
+
+            while (p.getLiga() != null) {
+
+                p = p.getLiga();
+            }
+            p.setLiga(x);
+
+        } else {
+
+            Punta = x;
+        }
+    }
+    public void InsertarFinalF(Nodo q,int fila,int colm, int dato) {
+
+        Nodo x = new Nodo(fila, colm, dato);
+
+        if (Punta != null) {
+
+            Nodo p = q;
+
+            while (p.getLf() != null) {
+
+                p = p.getLf();
+            }
+            p.setLf(x);
+
+        } else {
+
+            Punta = x;
+        }
+    }
     public void Paso1(int M[][]) {
 
-      int may, i = 0;
+        int may, i = 0;
 
-      if (M.length > M[0].length) {
+        if (M.length > M[0].length) {
 
-          may = M.length;
+            may = M.length;
 
-      } else {
+        } else {
 
-          may = M[0].length;
-      }
+            may = M[0].length;
+        }
+        
+        Punta = new Nodo(M.length, M[0].length, 0);
+        while (i < may) {
+            InsertarFinalRP(i,i,0);
+            i++;
+        }
 
-      while (i < may) {
-
-          Nodo p = new Nodo(i, i, 0);
-          i++;
-      }
-
-  }
+    }
 
     public void Paso2(int M[][]){
-        
-          
+        Nodo p=Punta.getLiga();
+        int i=0, j=0;
+        while (i<M.length) {
+            while (j<M[0].length) {
+                if(M[i][j]!=0){
+                    InsertarFinalF(p,i,j,M[i][j]);
+                }
+                j++;
+            }
+            j=0;
+            i++;
+            p=p.getLiga();
+        }
     }
 
     public void Paso3(){
