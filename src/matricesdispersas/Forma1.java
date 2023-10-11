40d5f0e683ca432dc77eb3bca6cae87a0428aacf
Andres Alvarez <andravid019@gmail.com>
2023-10-11 13:27:34 -0500

    cambio en insertar filas, avance en paso3, falta complementar

--- src/matricesdispersas/Forma1.java
+++ src/matricesdispersas/Forma1.java
@@ -2,17 +2,18 @@
 package matricesdispersas;
 
 public class Forma1 {
-    
+
     private Nodo Punta;
-    
-    public Forma1(){
-    Punta= null;
-    
+
+    public Forma1() {
+        Punta = null;
+
     }
-    public void Crear(int [][] M){
-      Paso1(M);
-      Paso2(M);
-      Paso3();
+
+    public void Crear(int[][] M) {
+        Paso1(M);
+        Paso2(M);
+        Paso3();
     }
 
     private void InsertarFinalRP(int fila, int colm, int dato) {
@@ -34,25 +35,25 @@ public class Forma1 {
             Punta = x;
         }
     }
-   
-    private void InsertarFinalF(Nodo q,int fila,int colm, int dato) {
+
+    private void InsertarFinalF(Nodo q, int fila, int colm, int dato) {
 
         Nodo x = new Nodo(fila, colm, dato);
-
-        if (Punta != null) {
-
-            Nodo p = q;
-            while (p.getLf() != null) {
+        Nodo p = q;
+        if (p.getLf() != null) {
+            while (p.getLf() != q) {
                 p = p.getLf();
             }
             p.setLf(x);
             x.setLf(q);
-
         } else {
-            Punta = x;
+            p.setLf(x);
+            x.setLf(q);
+
         }
+
     }
-    
+
     private void Paso1(int M[][]) {
 
         int may, i = 0;
@@ -65,58 +66,65 @@ public class Forma1 {
 
             may = M[0].length;
         }
-        
+
         Punta = new Nodo(M.length, M[0].length, 0);
-  Punta.setLiga(Punta);
+        Punta.setLiga(Punta);
         while (i < may) {
-            InsertarFinalRP(i,i,0);
+            InsertarFinalRP(i, i, 0);
             i++;
         }
 
     }
 
-    private void Paso2(int M[][]){
-          Nodo p=Punta.getLiga();
-          int i=0, j=0;
-          while (i<M.length) {
-              while (j<M[0].length) {
-                  if(M[i][j]!=0){
-                      InsertarFinalF(p,i,j,M[i][j]);
-                  }
-                  j++;
-              }
-              j=0;
-              i++;
-              p=p.getLiga();
-          }
-      }
-    
+    private void Paso2(int M[][]) {
+        Nodo p = Punta.getLiga();
+        int i = 0, j = 0;
+        while (i < M.length) {
+            while (j < M[0].length) {
+                if (M[i][j] != 0) {
+                    InsertarFinalF(p, i, j, M[i][j]);
+                }
+                j++;
+            }
+            j = 0;
+            i++;
+            p = p.getLiga();
+        }
+    }
 
-    private void Paso3(){
-        Nodo RC  = Punta.getLiga();
-        Nodo a = RC;
+    private void Paso3() {
+        Nodo RC = Punta.getLiga();
         Nodo p = Punta.getLiga();
         Nodo q = p.getLf();
+        Nodo a = RC;
 
-        while(RC!= Punta){
+        while (RC != Punta) {
+            while (p != Punta) {
+                q = p.getLf();
+             
+                while (q != p) {
+                 if(q!=null){
+                    if (q.getC() == RC.getC()) {
+                        a.setLc(q);
+                        a = q;
+                    }
+                    q = q.getLf();
+                }else { 
+                    p.setLf(p);
+                    q = p;
 
-          p = Punta.getLiga();
-            while(p!=Punta){
-              q = p.getLf();
-
-              while(q!=p){
-                if(q.getC() == RC.getC()){
-                  p.setLc(q);
                 }
-                q = q.getLf();
-              }
-              p=p.getLiga();
+                }
+                p = p.getLiga();
+            
+        }
+            if (p == Punta) {
+                a.setLc(RC);
 
             }
-
-            RC=RC.getLiga();
+            RC = RC.getLiga();
         }
-          
+
     }
-    
+
 }
