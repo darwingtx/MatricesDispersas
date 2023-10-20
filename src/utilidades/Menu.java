37dfbdbcc6e31a9c083ff8c26524266dc2b3331e
DarwinC <dc162005@gmail.com>
2023-10-19 22:09:21 -0500

    Adelante menu tripleta y eliminar dato en tripleta

--- src/utilidades/Menu.java
+++ src/utilidades/Menu.java
@@ -50,12 +50,12 @@ public class Menu {
                     + "\n1)Crear Matriz principal y tripleta"
                     + "\n2)Mostrar Tripleta"
                     + "\n3)Insertar Dato"
-                    + "\n3)Eliminar"
-                    + "\n3)Sumar Filas"
-                    + "\n4)Sumar Columnas"
-                    + "\n5)Sumar Tripletas"
-                    + "\n6)Multiplicar Tripletas"
-                    + "\n7)Multiplicar Tripleta y Forma 2 (T*F2 = F1)"
+                    + "\n4)Eliminar"
+                    + "\n5)Sumar Filas"
+                    + "\n6)Sumar Columnas"
+                    + "\n7)Sumar Tripletas"
+                    + "\n8)Multiplicar Tripletas"
+                    + "\n9)Multiplicar Tripleta y Forma 2 (T*F2 = F1)"
                     + "\n0) Salir"));
 
             switch (op) {
@@ -64,43 +64,90 @@ public class Menu {
                         n = Integer.parseInt(
                                 JOptionPane.showInputDialog(null, "¿Cuantas filas tendra la matriz?(Diferente de 0)"));
                         m = Integer.parseInt(
-                                JOptionPane.showInputDialog(null, "¿Cuantas filas tendra la matriz?(Diferente de 0)"));
-                    } while (n == 0 && m == 0);
+                                JOptionPane.showInputDialog(null,
+                                        "¿Cuantas columnas tendra la matriz?(Diferente de 0)"));
+                    } while (n <= 0 || m <= 0);
                     Mat = new int[n][m];
                     Mat = Utilidades.LlenadoAleatorio(n, m);
-                    T = new Tripleta(Utilidades.ContarDato(Mat));
+                    T = new Tripleta(n * m);
                     T.CrearTripleta(Mat);
                     break;
 
                 case 2:
-                    T.Mostrar();
+                    if (T != null) {
+                        T.Mostrar();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La tripleta no ha sido creada todavía.");
+                    }
                     break;
+
                 case 3:
-                    int dato =
+                    if (T != null) {
+                        int dato = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el dato a insertar:"));
+                        int fila = Integer
+                                .parseInt(JOptionPane.showInputDialog(null, "Ingrese la fila donde quiere insertar:"));
+                        int columna = Integer.parseInt(
+                                JOptionPane.showInputDialog(null, "Ingrese la columna donde quiere insertar:"));
+                        T.InsertarD(fila, columna, dato);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La tripleta no ha sido creada todavía.");
+                    }
                     break;
 
                 case 4:
-                    T.SumarColumna();
+                    int op2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Desea eliminar por: "
+                            + "\n1)Dato"
+                            + "\n2)Posicion"));
+
+                    switch (op2) {
+                        case 1:
+                            int dato = Integer
+                                    .parseInt(JOptionPane.showInputDialog(null, "Ingrese el dato a eliminar:"));
+                            T.EliminarD(dato);
+                            break;
+
+                        case 2:
+                            int fila = Integer.parseInt(
+                                    JOptionPane.showInputDialog(null, "Ingrese la fila donde quiere eliminar:"));
+                            int columna = Integer.parseInt(
+                                    JOptionPane.showInputDialog(null, "Ingrese la columna donde quiere eliminar:"));
+
+                             
+                            break;
+
+                        default:
+                            break;
+                    }
                     break;
 
                 case 5:
-                    do {
-                        n = Integer.parseInt(
-                                JOptionPane.showInputDialog(null, "¿Cuantas filas tendra la matriz?(Diferente de 0)"));
-                        m = Integer.parseInt(
-                                JOptionPane.showInputDialog(null, "¿Cuantas filas tendra la matriz?(Diferente de 0)"));
-                    } while (n == 0 && m == 0);
-                    Mat = new int[n][m];
-                    Mat = Utilidades.LlenadoAleatorio(n, m);
-                    
+                    if (T != null) {
+                        T.SumarFila();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La tripleta no ha sido creada todavía.");
+                    }
                     break;
 
                 case 6:
-
+                    if (T != null) {
+                        T.SumarColumna();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "La tripleta no ha sido creada todavía.");
+                    }
                     break;
 
                 case 7:
+                    
+                    break;
 
+                case 8:
+                    // Aquí deberías implementar la lógica para multiplicar dos tripletas si es
+                    // necesario.
+                    break;
+
+                case 9:
+                    // Aquí deberías implementar la lógica para multiplicar una tripleta y una forma
+                    // 2 si es necesario.
                     break;
 
                 default:
